6d44638643d57398fca60d5577e4ea83e0ddde45
Anahi Garcia Flores <anahigarciaflores1509@gmail.com>
2023-03-10 21:20:15 -0600

    complemente con el ejemplo de la clase, edite las converciones agregando las que faltaban y agregue los eventos en los dos combos

--- src/Temperaturas.java
+++ src/Temperaturas.java
@@ -2,31 +2,69 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import static java.awt.Font.PLAIN;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 public class Temperaturas extends JFrame{
 	GridBagConstraints gbc= new GridBagConstraints();
 	GridBagLayout gbl= new GridBagLayout();
-	JLabel a,txtResultado;
+	JLabel a,txtResultado,Convertir;
 	JTextArea txtGrados;
 	JComboBox<String> combTemp,combTemp2;
-	JButton btnConvertir;
-	
+	String temps[] = {"ºC", "ºF", "ºK", "ºR"};
+	 String g,g2;
 	public Temperaturas() {
 		getContentPane().setLayout(gbl); 
         setTitle("Conversoor temperaturas"); 
-        setSize(500,500);
+        setSize(800,300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         getContentPane().setBackground(Color.WHITE);
- 
-        JTextArea1(txtGrados," ",0,0,1,1);
-        JCombo(combTemp,"selesciona una opc.",1,0,1,2);
-		JLabel1(a,"A: ",0,1,1,1);
-		JCombo(combTemp2,"selesciona una opc.",1,1,1,1);
-		JBtn(btnConvertir,"Convertir",0,2,2,1);
-		JLabel1(txtResultado,"= ",0,3,2,1);
-		 
+       
+		JLabel1(Convertir,"convetir: ",0,0,1,1);
+		txtGrados = new JTextArea("0.0"); //Inicio JLabel
+		txtGrados.setOpaque(true); //Para poder darle un color de fondo
+		txtGrados.setBackground(Color.WHITE); //Color de fondo
+		txtGrados.setForeground(Color.BLUE); //Color de fuente
+		txtGrados.setBorder(new LineBorder(Color.DARK_GRAY)); //Borde
+		txtGrados.setFont(new Font("MONOSPACED", PLAIN, 40)); //Fuente
+		metodoMagico(txtGrados,1,0,1,1);
+       add(txtGrados); //Añado el JLabel al JFrame
+       
+        combTemp =new JComboBox<>();
+		combTemp.addItem("selesciona una opc.");
+		combTemp.addItem("ºC");
+		combTemp.addItem("ºF");
+		combTemp.addItem("ºK");
+		combTemp.addItem("ºR");
+		metodoMagico(combTemp,2,0,1,1);
+		add(combTemp); //Añado el JLabel al JFrame
+
+		
+		JLabel1(a,"A: ",0,2,1,1);
+		combTemp2 =new JComboBox<>();
+		combTemp2.addItem("selesciona una opc.");
+		combTemp2.addItem("ºC");
+		combTemp2.addItem("ºF");
+		combTemp2.addItem("ºK");
+		combTemp2.addItem("ºR");
+		metodoMagico(combTemp2,1,2,1,1);
+        add(combTemp2); //Añado el JLabel al JFrame
+        
+        txtResultado = new JLabel("= "); //Inicio JLabel
+		txtResultado.setOpaque(true); //Para poder darle un color de fondo
+		txtResultado.setBackground(Color.WHITE); //Color de fondo
+		txtResultado.setForeground(Color.BLUE); //Color de fuente
+		txtResultado.setFont(new Font("MONOSPACED", PLAIN, 40)); //Fuente
+		txtResultado.setHorizontalAlignment(SwingConstants.RIGHT); //Alineamiento horizontal derecha
+		metodoMagico(txtResultado,2,2,1,1);
+       add(txtResultado); //Añado el JLabel al JFrame
+		eventoOperacion();
+		
 		setVisible(true);
 	}//constructor
+	
 	public void metodoMagico(Component c,int x, int y, int w, int h) {
 		gbc.gridx=x;
 		gbc.gridy=y;
@@ -34,14 +72,6 @@ public class Temperaturas extends JFrame{
 		gbc.gridheight=h;
 		gbc.fill=GridBagConstraints.HORIZONTAL;
 		gbl.setConstraints(c, gbc);
-	}
-	public void JBtn(JButton c,String txt,int x, int y, int w, int h) {
-		c = new JButton(txt);
-		metodoMagico(c,x,y,w,h);
-		c.setFont(new Font("MONOSPACED",PLAIN,30));
-		c.setOpaque(true); 
-        c.setForeground(Color.BLACK);
-        add(c); //Añado el JBUTON al JFrame
 	}
 	public void JLabel1(JLabel c,String txt,int x, int y, int w, int h) {
 		c = new JLabel(txt); //Inicio JLabel
@@ -53,22 +83,92 @@ public class Temperaturas extends JFrame{
 		metodoMagico(c,x,y,w,h);
         add(c); //Añado el JLabel al JFrame
 	}
-	public void JCombo(JComboBox c,String txt,int x, int y, int w, int h) {
-		c =new JComboBox<>();
-		c.addItem("selesciona una opc.");
-		metodoMagico(c,x,y,w,h);
-        add(c); //Añado el JLabel al JFrame
-	}
-	public void JTextArea1(JTextArea c,String txt,int x, int y, int w, int h) {
-		c = new JTextArea(txt); //Inicio JLabel
-		 c.setOpaque(true); //Para poder darle un color de fondo
-		c.setBackground(Color.WHITE); //Color de fondo
-		c.setForeground(Color.BLUE); //Color de fuente
-		c.setBorder(new LineBorder(Color.DARK_GRAY)); //Borde
-		c.setFont(new Font("MONOSPACED", PLAIN, 40)); //Fuente
-		metodoMagico(c,x,y,w,h);
-        add(c); //Añado el JLabel al JFrame
-	}
+	
+	
+	 public void eventoOperacion(){
+		
+		 combTemp.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					g=combTemp.getSelectedItem().toString();
+					try {
+						llamadas(g,g2);
+					} catch (Exception e2) {
+						System.out.println("SOLO NUMEROS !!!!");
+					}
+				}
+			});
+		 combTemp2.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					g2=combTemp2.getSelectedItem().toString();
+					try {
+						llamadas(g,g2);
+					} catch (Exception e2) {
+						System.out.println("SOLO NUMEROS !!!!");
+					}
+					
+				}
+			});
+	    }
+	 public void llamadas(String g,String g2) {
+		 if(g=="ºC") {
+			 if(g2=="ºC") {
+					txtResultado.setText(txtGrados.getText());
+				}
+			if(g2=="ºF") {
+				txtResultado.setText(""+ConvertirTemp.convTemCAF(txtGrados.getText() ));
+			}
+			if(g2=="ºK") {
+				txtResultado.setText(""+ConvertirTemp.convTemCAK(txtGrados.getText() ));
+			}
+			if(g2=="ºR") {
+				txtResultado.setText(""+ConvertirTemp.convTemCAR(txtGrados.getText() ));
+			}
+		 }
+		 if(g=="ºF") {
+			 if(g2=="ºC") {
+				 	txtResultado.setText(""+ConvertirTemp.convTemFAC(txtGrados.getText() ));
+				}
+				if(g2=="ºF") {
+					txtResultado.setText(txtGrados.getText());
+				}
+				if(g2=="ºK") {
+					txtResultado.setText(""+ConvertirTemp.convTemFAK(txtGrados.getText() ));
+				}
+				if(g2=="ºR") {
+					txtResultado.setText(""+ConvertirTemp.convTemFAR(txtGrados.getText() ));
+				}
+			 }
+		 if(g=="ºK") {
+			 if(g2=="ºC") {
+					txtResultado.setText(""+ConvertirTemp.convTemKAC(txtGrados.getText() ));
+				}
+				if(g2=="ºF") {
+					txtResultado.setText(""+ConvertirTemp.convTemKAF(txtGrados.getText() ));
+				}
+				if(g2=="ºK") {
+					txtResultado.setText(txtGrados.getText());
+				}
+				if(g2=="ºR") {
+					txtResultado.setText(""+ConvertirTemp.convTemKAR(txtGrados.getText() ));
+				}
+			 }
+		 if(g=="ºR") {
+			 if(g2=="ºC") {
+				 txtResultado.setText(""+ConvertirTemp.convTemRAC(txtGrados.getText() ));
+				}
+				if(g2=="ºF") {
+					txtResultado.setText(""+ConvertirTemp.convTemRAF(txtGrados.getText() ));
+				}
+				if(g2=="ºK") {
+					txtResultado.setText(""+ConvertirTemp.convTemRAK(txtGrados.getText() ));
+				}
+				if(g2=="ºR") {
+					txtResultado.setText(txtGrados.getText());
+				}
+			 }
+	 }
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
